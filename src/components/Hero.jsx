ae031464fa61ffb9c0508a500587cc4009a79a1e
Aditya Tamgade <aditya.tamgade@gmail.com>
2025-07-31 01:05:04 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
   };
 
   return (
-    <section className="relative w-full min-h-screen flex flex-col justify-center items-center text-center px-6 sm:px-12 md:px-20 lg:px-60 overflow-visible pt-32 mt-10">
+    <section className="relative w-full min-h-screen flex flex-col justify-center items-center text-center px-6 sm:px-12 md:px-20 lg:px-60 overflow-visible mt-32">
       {/* Background Image */}
       <div
         className="absolute inset-0 bg-cover bg-center z-0 opacity-20 sm:opacity-30 md:opacity-40"
@@ -28,10 +28,10 @@ export default function Hero() {
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
-          className="text-3xl sm:text-6xl font-bold text-white leading-tight"
+          className="text-3xl sm:text-5xl font-bold text-white leading-tight"
         >
           Hi, I'm{" "}
-          <span className=" mt-8 text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-purple-500">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-purple-500">
             Aditya Tamgade
           </span>
         </motion.h1>
@@ -41,7 +41,7 @@ export default function Hero() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.4, duration: 1 }}
-          className="text-lg sm:text-3xl text-gray-300 mt-4"
+          className="text-lg sm:text-3xl text-gray-300 mt-2"
         >
           A Passionate{" "}
           <span className="text-indigo-300">
@@ -54,11 +54,10 @@ export default function Hero() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.7, duration: 1 }}
-          className="text-gray-400 max-w-2xl mt-6 text-base sm:text-lg"
+          className="text-gray-400 max-w-2xl mt-2 text-base sm:text-lg justify-center"
         >
           I design and build modern, engaging web interfaces with top-notch user
-          experiences. Currently exploring AI integrations, clean UI/UX, and
-          real-time applications.
+          experiences. Currently exploring AI integrations.
         </motion.p>
 
         {/* Buttons */}
@@ -66,15 +65,15 @@ export default function Hero() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1, duration: 1 }}
-          className="flex gap-6 mt-8 flex-wrap justify-center"
+          className="flex gap-6 mt-5 flex-wrap justify-center"
         >
           <Link to="/work">
-            <button className="px-6 py-3 border border-white text-white hover:bg-white hover:text-black transition rounded-full shadow-md">
+            <button className="px-4 py-2 border border-white text-white hover:bg-white hover:text-black transition rounded-full shadow-md">
               View Projects
             </button>
           </Link>
           <Link to="/contact">
-            <button className="px-6 py-3 bg-indigo-600 hover:bg-indigo-500 transition rounded-full text-white shadow-md">
+            <button className="px-4 py-2 bg-indigo-600 hover:bg-indigo-500 transition rounded-full text-white shadow-md">
               Hire Me
             </button>
           </Link>
@@ -85,7 +84,7 @@ export default function Hero() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, y: [0, 10, 0] }}
           transition={{ delay: 2, duration: 2, repeat: Infinity }}
-          className="mt-6 cursor-pointer"
+          className=" cursor-pointer"
           onClick={scrollToEducation}
         >
           <span className="text-indigo-500 text-3xl animate-bounce">↓</span>
@@ -95,11 +94,11 @@ export default function Hero() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 1, duration: 1 }}
-          className="flex gap-6 mt-5 flex-wrap justify-center"
+          className="flex gap-6 flex-wrap justify-center"
         >
           <Link
             to="/qualification"
-            className="mt-2 inline-flex items-center gap-2 bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-purple-600 hover:to-indigo-500 text-white font-semibold px-5 py-2 rounded-full shadow-md hover:scale-105 transition-transform duration-300"
+            className="mt-4 inline-flex items-center gap-2 bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-purple-600 hover:to-indigo-500 text-white font-semibold px-4 py-2 rounded-full shadow-md hover:scale-105 transition-transform duration-300"
           >
             Experience and Qualifications
           </Link>

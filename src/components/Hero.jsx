51524eefe0c668d662bab20af956e77360159246
Aditya Tamgade <aditya.tamgade@gmail.com>
2026-04-21 12:03:12 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -182,29 +182,30 @@ export default function Hero() {
 
         
         {/* Scroll Indicator */}
-{/* // { <motion.div */}
-//   initial={{ opacity: 0 }}
-//   animate={{ opacity: 1 }}
-//   transition={{ delay: 1.5, duration: 1 }}
-//   className="mt-6 flex flex-col items-center gap-2 cursor-pointer"
-//   onClick={scrollToEducation}
-// >
-//   <span className="text-gray-400 text-xs tracking-widest uppercase">
-//     Scroll
-//   </span>
+        
+{/* <motion.div 
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{ delay: 1.5, duration: 1 }}
+  className="mt-6 flex flex-col items-center gap-2 cursor-pointer"
+  onClick={scrollToEducation}
+>
+  <span className="text-gray-400 text-xs tracking-widest uppercase">
+    Scroll
+  </span>
 
-//   <motion.div
-//     animate={{ y: [0, 12, 0] }}
-//     transition={{ duration: 1.5, repeat: Infinity }}
-//     className="w-6 h-10 border-2 border-indigo-400 rounded-full flex items-start justify-center p-1"
-//   >
-//     <motion.div
-//       animate={{ y: [0, 12, 0] }}
-//       transition={{ duration: 1.5, repeat: Infinity }}
-//       className="w-1 h-2 bg-indigo-400 rounded-full"
-//     />
-//   </motion.div>
-// </motion.div> } 
+  <motion.div
+    animate={{ y: [0, 12, 0] }}
+    transition={{ duration: 1.5, repeat: Infinity }}
+    className="w-6 h-10 border-2 border-indigo-400 rounded-full flex items-start justify-center p-1"
+  >
+    <motion.div
+      animate={{ y: [0, 12, 0] }}
+      transition={{ duration: 1.5, repeat: Infinity }}
+      className="w-1 h-2 bg-indigo-400 rounded-full"
+    />
+  </motion.div>
+</motion.div> }  */}
 
         {/* Qualification Button */}
         <motion.div
